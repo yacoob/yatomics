8d53a17b5307f7108ef5ee2521be63868856efca
Jakub Turski <yacoob@ftml.net>
2024-11-23 18:33:39 +0000

    feat: add podlet binary

--- files/scripts/eget.sh
+++ files/scripts/eget.sh
@@ -5,7 +5,8 @@ curl https://zyedidia.github.io/eget.sh | sh
 install eget /usr/bin
 rm -f eget
 # use eget to install other binaries
-/usr/bin/eget atanunq/viu --to=/usr/bin
-/usr/bin/eget atuinsh/atuin -a gnu -a '^update' --to=/usr/bin
-/usr/bin/eget creativeprojects/resticprofile -a 'no_self_update' --to=/usr/bin
-/usr/bin/eget watchexec/watchexec -a musl.tar.xz _ -a '^b3' -a '^sha512' --to=/usr/bin
+eget atanunq/viu --to=/usr/bin
+eget atuinsh/atuin -a gnu -a '^update' --to=/usr/bin
+eget containers/podlet --to=/usr/bin -a gnu
+eget creativeprojects/resticprofile -a no_self_update --to=/usr/bin
+eget watchexec/watchexec -a musl.tar.xz _ -a '^b3' -a '^sha512' --to=/usr/bin
