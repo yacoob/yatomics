d7aba3c27ede762cec9eb2aebb1ac6eec7685364
Jakub Turski <yacoob@ftml.net>
2024-11-29 18:40:34 +0000

    feat: add tgsend, use it for the failure-reporting systemd unit

--- files/scripts/eget.sh
+++ files/scripts/eget.sh
@@ -9,4 +9,5 @@ eget atanunq/viu --to=/usr/bin
 eget atuinsh/atuin -a gnu -a '^update' --to=/usr/bin
 eget containers/podlet --to=/usr/bin -a gnu
 eget creativeprojects/resticprofile -a no_self_update --to=/usr/bin
+eget stunnel/tgsend -f 'tgsend-*' --to=/usr/bin/tgsend
 eget watchexec/watchexec -a musl.tar.xz _ -a '^b3' -a '^sha512' --to=/usr/bin
