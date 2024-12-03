760df9434a8112f3c615ee9e952a497cab14e803
Jakub Turski <yacoob@ftml.net>
2024-12-03 17:29:01 +0000

    feat: add chezmoi to both images

--- files/scripts/eget.sh
+++ files/scripts/eget.sh
@@ -10,4 +10,5 @@ eget atuinsh/atuin -a gnu -a '^update' --to=/usr/bin
 eget containers/podlet --to=/usr/bin -a gnu
 eget creativeprojects/resticprofile -a no_self_update --to=/usr/bin
 eget stunnel/tgsend -f 'tgsend-*' --to=/usr/bin/tgsend
+eget VorpalBlade/chezmoi_modify_manager -a gnu --to=/usr/bin
 eget watchexec/watchexec -a musl.tar.xz _ -a '^b3' -a '^sha512' --to=/usr/bin
