15845b1c7d787e340552cbe03e319bc94c544695
Jakub Turski <yacoob@ftml.net>
2024-12-06 00:33:05 +0000

    feat: add dive to both images

--- files/scripts/eget.sh
+++ files/scripts/eget.sh
@@ -11,4 +11,5 @@ eget containers/podlet --to=/usr/bin -a gnu
 eget creativeprojects/resticprofile -a no_self_update --to=/usr/bin
 eget stunnel/tgsend -f 'tgsend-*' --to=/usr/bin/tgsend
 eget VorpalBlade/chezmoi_modify_manager -a gnu --to=/usr/bin
+eget wagoodman/dive -a tar --to=/usr/bin
 eget watchexec/watchexec -a musl.tar.xz _ -a '^b3' -a '^sha512' --to=/usr/bin
