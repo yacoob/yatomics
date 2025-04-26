fe7348a397995cc76cc0a7a75aa688cbc18de7df
Jakub Turski <yacoob@ftml.net>
2025-04-26 23:24:51 +0100

    chore: update RPM packages
    Install atuin and podlet from fedora itself. Stop using copr for gdu.

--- files/scripts/eget.sh
+++ files/scripts/eget.sh
@@ -5,8 +5,6 @@ curl https://zyedidia.github.io/eget.sh | sh
 install -o root -g root eget /usr/bin
 rm -f eget
 # use eget to install other binaries
-eget atuinsh/atuin -a gnu -a '^update' --to=/usr/bin
-eget containers/podlet --to=/usr/bin -a gnu
 eget creativeprojects/resticprofile -a no_self_update --to=/usr/bin
 eget loft-sh/devpod -a amd64 -a devpod --to=/usr/bin
 eget stunnel/tgsend -f 'tgsend-*' --to=/usr/bin/tgsend
