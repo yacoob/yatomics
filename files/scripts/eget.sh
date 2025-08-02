9086d0e634f04d49daf4be958cae7d10cece27d5
Jakub Turski <yacoob@ftml.net>
2025-08-02 14:45:08 +0100

    feat: use curl for ntfy in systemd's unit
    This removes the need for tgsend and ntfy binary.
    
    eget binary itself is still needed at image build time, as I use it for
    neovide in yaurora.

--- files/scripts/eget.sh
+++ files/scripts/eget.sh
@@ -4,6 +4,3 @@ set -euo pipefail
 curl https://zyedidia.github.io/eget.sh | sh
 install -o root -g root eget /usr/bin
 rm -f eget
-# use eget to install other binaries
-eget binwiederhier/ntfy -a tar.gz --to=/usr/bin
-eget stunnel/tgsend -f 'tgsend-*' --to=/usr/bin/tgsend
