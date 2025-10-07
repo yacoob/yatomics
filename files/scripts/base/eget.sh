82326e07a8a201bc977ccf61a01b8040d8e376a4
Jakub Turski <yacoob@ftml.net>
2025-10-07 14:14:04 +0100

    cleanup: remove eget in favour of ubi
    Note: this breaks chezmoi past this commit, as it needs
    chezmoi_modify_manager right now.

--- files/scripts/base/eget.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-# install eget via its installer
-curl https://zyedidia.github.io/eget.sh | sh
-install -o root -g root eget /usr/bin
-rm -f eget
-# install binaries via eget - while the preference is for users to use brew/soar
-# some binaries are important enough to warrant a place in /usr/bin
-eget VorpalBlade/chezmoi_modify_manager -a gnu --to=/usr/bin
